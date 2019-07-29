22d54cb0464888cbf7d2909bd60dd4114fb84491
Joan Andrés <andres6936@live.com>
2019-07-28 22:18:48 -0500

    Add Platform Module

---
+++ Include/Plataform/Unix.hpp
@@ -0,0 +1,13 @@
+//
+
+#ifndef LIBTCOD_UNIX_HPP
+#define LIBTCOD_UNIX_HPP
+
+
+class Unix
+{
+
+};
+
+
+#endif //LIBTCOD_UNIX_HPP
---
+++ Include/Plataform/Windows.hpp
@@ -0,0 +1,13 @@
+//
+
+#ifndef LIBTCOD_WINDOWS_HPP
+#define LIBTCOD_WINDOWS_HPP
+
+
+class Windows
+{
+
+};
+
+
+#endif //LIBTCOD_WINDOWS_HPP
