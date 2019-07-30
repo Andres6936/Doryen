88e61960380493f62b10b10bee7d2ed39652a03e
Joan Andrés <andres6936@live.com>
2019-07-30 13:08:19 -0500

    Separated definition from implementation in LineListener

--- Samples/CPP/LineListener.cpp
+++ Samples/CPP/LineListener.cpp
@@ -1,1 +1,11 @@
 #include "LineListener.hpp"
+
+bool LineListener::putPoint( int x, int y )
+{
+    if ( x >= 0 && y >= 0 && x < SAMPLE_SCREEN_WIDTH && y < SAMPLE_SCREEN_HEIGHT )
+    {
+        sampleConsole.setCharBackground( x, y, Doryen::Color::lightBlue, ( TCOD_bkgnd_flag_t ) bkFlag );
+    }
+
+    return true;
+}
--- Samples/CPP/LineListener.hpp
+++ Samples/CPP/LineListener.hpp
@@ -7,15 +7,10 @@
 
 class LineListener : public TCODLineListener
 {
+
 public :
-    bool putPoint( int x, int y )
-    {
-        if ( x >= 0 && y >= 0 && x < SAMPLE_SCREEN_WIDTH && y < SAMPLE_SCREEN_HEIGHT )
-        {
-            sampleConsole.setCharBackground( x, y, Doryen::Color::lightBlue, ( TCOD_bkgnd_flag_t ) bkFlag );
-        }
-        return true;
-    }
+
+    bool putPoint( int x, int y );
 };
 
 
