f6c9f298277e6bb4ac21f129fee236293814d925
Joan Andrés <andres6936@live.com>
2019-08-02 11:36:30 -0500

    Add new Enum, Direction

---
+++ Include/Enum/Direction.hpp
@@ -0,0 +1,20 @@
+#ifndef LIBTCOD_DIRECTION_HPP
+#define LIBTCOD_DIRECTION_HPP
+
+namespace Doryen
+{
+    enum class Direction : short
+    {
+        NORTH_WEST,
+        NORTH,
+        NORTH_EAST,
+        WEST,
+        NONE,
+        EAST,
+        SOUTH_WEST,
+        SOUTH,
+        SOUTH_EAST
+    };
+}
+
+#endif //LIBTCOD_DIRECTION_HPP
