a8bcb7986d509740237edc24212000ee7767cf68
Joan Andrés <andres6936@live.com>
2019-08-16 14:06:52 -0500

    Implementing AStar {Added Node class}

---
+++ Include/Algorithms/Util/Node.hpp
@@ -0,0 +1,26 @@
+#ifndef LIBTCOD_NODE_HPP
+#define LIBTCOD_NODE_HPP
+
+namespace Doryen
+{
+    namespace Algorithms
+    {
+        class Node
+        {
+
+        private:
+
+            int x;
+            int y;
+
+        public:
+
+            Node();
+
+            Node(int X, int Y);
+
+        };
+    }
+}
+
+#endif //LIBTCOD_NODE_HPP
---
+++ Source/Algorithms/Util/Node.cpp
@@ -0,0 +1,14 @@
+#include "Algorithms/Util/Node.hpp"
+
+
+Doryen::Algorithms::Node::Node( )
+{
+    x = 0;
+    y = 0;
+}
+
+Doryen::Algorithms::Node::Node( int X, int Y )
+{
+    x = X;
+    y = Y;
+}
