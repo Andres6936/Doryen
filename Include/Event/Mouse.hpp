d3cb12e2f29a17e9616b7ea8a303849679676d91
Joan Andrés <andres6936@live.com>
2020-05-28 22:41:28 -0500

    Implemented class Mouse

--- Include/Event/Mouse.hpp
+++ Include/Event/Mouse.hpp
@@ -3,7 +3,44 @@
 
 namespace Doryen
 {
+	enum class MouseCode : short
+	{
+		NONE,
 
+		LEFT,
+		MIDDLE,
+		RIGHT,
+
+		WHEEL_UP,
+		WHEEL_DOWN,
+	};
+
+	class Mouse
+	{
+
+	private:
+
+		int x = 0;
+		int y = 0;
+
+		int dx = 0;
+		int dy = 0;
+
+		int cx = 0;
+		int cy = 0;
+
+		int dcx = 0;
+		int dcy = 0;
+
+		/**
+		 * Determine if the button left, right, middle was pressed.
+		 * Determine if wheel up or wheel down was pressed (aka: moved).
+		 */
+		MouseCode status = MouseCode::NONE;
+
+	public:
+
+	};
 }
 
 #endif //LIBTCOD_MOUSE_HPP
