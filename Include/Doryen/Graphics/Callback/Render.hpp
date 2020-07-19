32adeabb9406bf0cefa1de091d8b12dc894e9990
Joan Andrés <andres6936@live.com>
2020-07-19 13:44:42 -0500

    Create class CallbackRender

---
+++ Include/Doryen/Graphics/Callback/Render.hpp
@@ -0,0 +1,24 @@
+// Joan Andrés (@Andres6936) Github.
+
+#ifndef DORYEN_RENDER_HPP
+#define DORYEN_RENDER_HPP
+
+#include <any>
+
+namespace Doryen
+{
+
+	class CallbackRender
+	{
+
+	public:
+
+		virtual ~CallbackRender() = default;
+
+		virtual void render(std::any& _surface) = 0;
+
+	};
+
+}
+
+#endif //DORYEN_RENDER_HPP
