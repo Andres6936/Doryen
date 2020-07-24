fa2ef2165985dcf4274d30c4700fd33118bcfce3
Joan Andrés <andres6936@live.com>
2020-07-24 11:41:36 -0500

    Added new test case

--- Test/Overflow/TestColor.cpp
+++ Test/Overflow/TestColor.cpp
@@ -123,3 +123,42 @@ TEST_CASE ("Overflow for operations of multiply for a scalar negative")
 
 			CHECK(blue.equals({ 0, 0, 0 }));
 }
+
+TEST_CASE ("Overflow for operations of add for another color")
+{
+	// Element Null
+	Color white{ 0, 0, 0 };
+	white.add({ 255, 255, 255 });
+
+			CHECK(white.equals({ 255, 255, 255 }));
+
+	// Element Neutral
+	Color neutral{ 1, 1, 1 };
+	neutral.add({ 255, 255, 255 });
+
+			CHECK(neutral.equals({ 255, 255, 255 }));
+
+	// Element Total
+	Color black{ 255, 255, 255 };
+	black.add({ 255, 255, 255 });
+
+			CHECK(black.equals({ 255, 255, 255 }));
+
+	// Element Component Red
+	Color red{ 255, 0, 0 };
+	red.add({ 255, 0, 0 });
+
+			CHECK(red.equals({ 255, 0, 0 }));
+
+	// Element Component Green
+	Color green{ 0, 255, 0 };
+	green.add({ 0, 255, 0 });
+
+			CHECK(green.equals({ 0, 255, 0 }));
+
+	// Element Component Blue
+	Color blue{ 0, 0, 255 };
+	blue.add({ 0, 0, 255 });
+
+			CHECK(blue.equals({ 0, 0, 255 }));
+}
