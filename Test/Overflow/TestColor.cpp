22afc3d248bbc03b8f315a28298641e3d189acf7
Joan Andrés <andres6936@live.com>
2020-07-24 11:34:23 -0500

    Added test for multiply for scalar positive and negative

--- Test/Overflow/TestColor.cpp
+++ Test/Overflow/TestColor.cpp
@@ -46,3 +46,82 @@ TEST_CASE ("Overflow for operations of multiply for another color")
 
 			CHECK(blue.equals({ 0, 0, 255 }));
 }
+
+TEST_CASE ("Overflow for operations of multiply for a scalar positive")
+{
+	// Element Null
+	Color white{ 0, 0, 0 };
+	white.multiply(255.0f);
+
+			CHECK(white.equals({ 0, 0, 0 }));
+
+	// Element Neutral
+	Color neutral{ 1, 1, 1 };
+	neutral.multiply(255.0f);
+
+			CHECK(neutral.equals({ 255, 255, 255 }));
+
+	// Element Total
+	Color black{ 255, 255, 255 };
+	black.multiply(255.0f);
+
+			CHECK(black.equals({ 255, 255, 255 }));
+
+	// Element Component Red
+	Color red{ 255, 0, 0 };
+	red.multiply(255.0f);
+
+			CHECK(red.equals({ 255, 0, 0 }));
+
+	// Element Component Green
+	Color green{ 0, 255, 0 };
+	green.multiply(255.0f);
+
+			CHECK(green.equals({ 0, 255, 0 }));
+
+	// Element Component Blue
+	Color blue{ 0, 0, 255 };
+	blue.multiply(255.0f);
+
+			CHECK(blue.equals({ 0, 0, 255 }));
+}
+
+
+TEST_CASE ("Overflow for operations of multiply for a scalar negative")
+{
+	// Element Null
+	Color white{ 0, 0, 0 };
+	white.multiply(-255.0f);
+
+			CHECK(white.equals({ 0, 0, 0 }));
+
+	// Element Neutral
+	Color neutral{ 1, 1, 1 };
+	neutral.multiply(-255.0f);
+
+			CHECK(neutral.equals({ 0, 0, 0 }));
+
+	// Element Total
+	Color black{ 255, 255, 255 };
+	black.multiply(-255.0f);
+
+			CHECK(black.equals({ 0, 0, 0 }));
+
+	// Element Component Red
+	Color red{ 255, 0, 0 };
+	red.multiply(-255.0f);
+
+			CHECK(red.equals({ 0, 0, 0 }));
+
+	// Element Component Green
+	Color green{ 0, 255, 0 };
+	green.multiply(-255.0f);
+
+			CHECK(green.equals({ 0, 0, 0 }));
+
+	// Element Component Blue
+	Color blue{ 0, 0, 255 };
+	blue.multiply(-255.0f);
+
+			CHECK(blue.equals({ 0, 0, 0 }));
+}
