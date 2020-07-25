5bdea497a9bcc1b2b81e0b27a24d8d4b1c74990a
Joan Andrés <andres6936@live.com>
2020-07-24 22:34:00 -0500

    Deleted function TCOD_image_scale

--- Include/Doryen/Image.h
+++ Include/Doryen/Image.h
@@ -47,8 +47,6 @@ void TCOD_image_rotate90(TCOD_image_t image, int numRotations);
 
 void TCOD_image_vflip(TCOD_image_t image);
 
-void TCOD_image_scale(TCOD_image_t image, int neww, int newh);
-
 void TCOD_image_save(TCOD_image_t image, const char *filename);
 
 void TCOD_image_get_size(TCOD_image_t image, int *w,int *h);
