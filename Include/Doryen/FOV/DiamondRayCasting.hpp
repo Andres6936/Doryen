9f0d3addace1a648ed44b969cfeb7a0f2dc77311
Joan Andrés <andres6936@live.com>
2020-07-25 09:05:28 -0500

    First steps for implement the Diamond Raycasting

--- Include/Doryen/FOV/DiamondRayCasting.hpp
+++ Include/Doryen/FOV/DiamondRayCasting.hpp
@@ -1,12 +1,45 @@
 #ifndef LIBTCOD_DIAMONDRAYCASTING_HPP
 #define LIBTCOD_DIAMONDRAYCASTING_HPP
 
+#include <cstdint>
+
 #include <Doryen/FOV/Fov.hpp>
+#include <Doryen/Geometry/Point2D.hpp>
 
 namespace Doryen
 {
 	class DiamondRayCasting
 	{
+
+	private:
+
+		class RayData
+		{
+			/**
+			 * Position in coordinate x and y.
+			 */
+			Geometry::Point2D<> position{};
+
+			/**
+			 * Obscurity vector.
+			 */
+			Geometry::Point2D<> obscurity{};
+
+			/**
+			 * Bresenham error.
+			 */
+			Geometry::Point2D<> bresenham{};
+
+			/**
+			 * Already in the field of view.
+			 */
+			bool added = false;
+
+			/**
+			 * Not visible. Don't bother processing it.
+			 */
+			bool ignore = false;
+		};
 
 	public:
 
--- Source/FOV/DiamondRayCasting.cpp
+++ Source/FOV/DiamondRayCasting.cpp
@@ -1,6 +1,14 @@
 #include "Doryen/FOV/DiamondRayCasting.hpp"
 
-void Doryen::DiamondRayCasting::operator()(Doryen::Map& map, int playerX, int playerY, int maxRadius, bool ligthWalls)
+void Doryen::DiamondRayCasting::operator()(
+		Doryen::Map& map, int playerX, int playerY,
+		int maxRadius, bool ligthWalls)
 {
+	const std::int32_t r2 = maxRadius * maxRadius;
+
+	// Field of view origin
+	const std::int32_t originX = playerX;
+	const std::int32_t originY = playerY;
+
 
 }
