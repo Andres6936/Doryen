657353ba74205544049abf3ef765ef833e4a2287
Joan Andrés <andres6936@live.com>
2020-07-24 22:12:33 -0500

    Deleted unused macros

--- Source/sys_c.c
+++ Source/sys_c.c
@@ -45,27 +45,6 @@
 #include <dirent.h>
 #endif
 
-#if defined(TCOD_WINDOWS)
-char *strcasestr (const char *haystack, const char *needle) {
-	const char *p, *startn = 0, *np = 0;
-
-	for (p = haystack; *p; p++) {
-		if (np) {
-			if (toupper(*p) == toupper(*np)) {
-				if (!*++np)
-					return (char *)startn;
-			} else
-				np = 0;
-		} else if (toupper(*p) == toupper(*needle)) {
-			np = needle + 1;
-			startn = p;
-		}
-	}
-
-	return 0;
-}
-#endif
-
 
 static bool filename_match(const char *name, const char *pattern) {
 	char *ptr;
