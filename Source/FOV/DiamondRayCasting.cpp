92f98b5af72d0c3d68365f0f832b75e111d76248
Joan Andrés <andres6936@live.com>
2020-07-27 09:18:00 -0500

    Added documentation

--- Source/FOV/DiamondRayCasting.cpp
+++ Source/FOV/DiamondRayCasting.cpp
@@ -23,6 +23,7 @@ DiamondRayCasting::compute(std::uint32_t octant, Geometry::Point2D<> origin,
 		std::uint32_t rangeLimit, std::uint32_t x,
 		DiamondRayCasting::Slope top, DiamondRayCasting::Slope bottom)
 {
+	// rangeLimit < 0 || x <= rangeLimit
 	for (; x <= rangeLimit; ++x)
 	{
 		std::int32_t topY = 0;
@@ -33,33 +34,41 @@ DiamondRayCasting::compute(std::uint32_t octant, Geometry::Point2D<> origin,
 		}
 		else
 		{
+			// get the tile that the top vector enters from the left
 			topY = ((x * 2 - 1) * top.y + top.x) / (top.x * 2);
 			std::int32_t ay = (topY * 2 + 1) * top.x;
 
+			// if the top tile is a wall...
 			if (blocksLight(x, topY, octant, origin))
 			{
 				if (top.greaterOrEqual(ay, x * 2))
 				{
+					// but the top vector misses the wall and passes into the tile above, move up
 					topY += 1;
 				}
 			}
+				// the top tile is not a wall
 			else
 			{
 				if (top.greater(ay, x * 2 + 1))
 				{
+					// so if the top vector passes into the tile above, move up
 					topY += 1;
 				}
 			}
 		}
 
 		std::int32_t bottomY = bottom.y == 0 ? 0 : ((x * 2 - 1) * bottom.y + bottom.x) / (bottom.x * 2);
-		std::int32_t wasOpaque = -1; // 0:false, 1:true, -1:not applicable
+
+		// 0:false, 1:true, -1:not applicable
+		std::int32_t wasOpaque = -1;
 
 		for (int y = topY; y >= bottomY; --y)
 		{
 			std::int32_t tx = origin.x, ty = origin.y;
 
-			switch (octant) // translate local coordinates to map coordinates
+			// translate local coordinates to map coordinates
+			switch (octant)
 			{
 			case 0:
 				tx += x;
@@ -116,8 +125,11 @@ DiamondRayCasting::compute(std::uint32_t octant, Geometry::Point2D<> origin,
 			{
 				if (isOpaque)
 				{
+					// if we found a transition from clear to opaque, this sector is done in this column, so
+					// adjust the bottom vector upwards and continue processing it in the next column.
 					if (wasOpaque == 0)
 					{
+						// (x * 2 - 1, y * 2 + 1) is a vector to the top-left corner of the opaque block
 						if (!inRange || y == bottomY)
 						{
 							bottom = { y * 2 + 1, static_cast<int32_t>(x * 2) };
@@ -133,8 +145,11 @@ DiamondRayCasting::compute(std::uint32_t octant, Geometry::Point2D<> origin,
 
 					wasOpaque = 1;
 				}
+					// adjust the top vector downwards and continue if we found a transition from opaque to clear
 				else
 				{
+					// (x * 2 + 1, y * 2 + 1) is the top-right corner of the clear tile (i.e. the bottom-right
+					// of the opaque tile)
 					if (wasOpaque > 0)
 					{
 						top = { y * 2 + 1, static_cast<int32_t>(x * 2) };
@@ -145,6 +160,7 @@ DiamondRayCasting::compute(std::uint32_t octant, Geometry::Point2D<> origin,
 			}
 		}
 
+		// if the column ended in a clear tile, continue processing the current sector
 		if (wasOpaque not_eq 0) break;
 	}
 }
