691ce31171ea5122b5a20c8540eb79bb82e31575
Joan Andrés <andres6936@live.com>
2020-07-27 11:24:42 -0500

    Step I: Support to calculate Permissive

--- Include/Doryen/FOV/Permissive.hpp
+++ Include/Doryen/FOV/Permissive.hpp
@@ -1,6 +1,11 @@
 #ifndef LIBTCOD_PERMISSIVE_HPP
 #define LIBTCOD_PERMISSIVE_HPP
 
+#include <memory>
+#include <vector>
+#include <cstdint>
+
+#include "Doryen/Geometry/Point2D.hpp"
 #include <Doryen/FOV/Fov.hpp>
 
 namespace Doryen
@@ -8,6 +13,51 @@ namespace Doryen
 
 	class Permissive
 	{
+
+	private:
+
+		class ViewBump final : public Geometry::Point2D<>
+		{
+
+		public:
+
+			std::int32_t refCount{ 0 };
+
+			std::shared_ptr<ViewBump> parent{ nullptr };
+
+		};
+
+		class Line final
+		{
+
+		public:
+
+			std::int32_t xi{ 0 };
+			std::int32_t yi{ 0 };
+
+			std::int32_t xf{ 0 };
+			std::int32_t yf{ 0 };
+
+		};
+
+		class View final
+		{
+
+		public:
+
+			Line shallowLine;
+
+			Line steepLine;
+
+			std::vector<ViewBump> shallowBump;
+
+			std::vector<ViewBump> steepBump;
+
+		};
+
+		std::vector<View> views;
+
+		std::vector<ViewBump> bumps;
 
 	public:
 
--- Source/FOV/Permissive.cpp
+++ Source/FOV/Permissive.cpp
@@ -3,5 +3,39 @@
 void
 Doryen::Permissive::operator()(Doryen::Map& map, int playerX, int playerY, int maxRadius, bool ligthWalls, int fovType)
 {
+	// Defines the parameters of the permissiveness.
+	// Derived values defining the actual part of the square used as a range.
+	std::int32_t offset = 8 - fovType;
+	std::int32_t limit = 8 + fovType;
 
+	map.resetFieldView();
+	map.setVisibleFieldView(playerX, playerY);
+
+	views.resize(map.getWidth() * map.getHeight());
+	bumps.resize(map.getWidth() * map.getHeight());
+
+	std::int32_t minX {0};
+	std::int32_t maxX {0};
+
+	std::int32_t minY {0};
+	std::int32_t maxY {0};
+
+	if (maxRadius > 0)
+	{
+		minX = std::min(playerX, maxRadius);
+		maxX = std::min(map.getWidth() - playerX - 1, maxRadius);
+
+		minY = std::min(playerY, maxRadius);
+		maxY = std::min(map.getHeight() - playerY - 1, maxRadius);
+	}
+	else
+	{
+		minX = playerX;
+		maxX = map.getWidth() - playerX - 1;
+
+		minY = playerY;
+		maxY = map.getHeight() - playerY - 1;
+	}
+
+	// Calculate fov. precise permissive field of view
 }
