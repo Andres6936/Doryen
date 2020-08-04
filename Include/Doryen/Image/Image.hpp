0c56e62ea4b736301015a0fc0a71adfd3ed67857
Joan Andrés <andres6936@live.com>
2020-08-04 14:01:02 -0500

    Deleted unused construct

--- Include/Doryen/Image/Image.hpp
+++ Include/Doryen/Image/Image.hpp
@@ -410,12 +410,6 @@ namespace Doryen
 		*/
 		void blit2x(Console& dest, int dx, int dy, int sx = 0, int sy = 0, int w = -1, int h = -1) const;
 
-		explicit Image(TCOD_image_t img)
-		{
-			this->data = img;
-			this->deleteData = false;
-		}
-
 		virtual ~Image();
 
 	private:
