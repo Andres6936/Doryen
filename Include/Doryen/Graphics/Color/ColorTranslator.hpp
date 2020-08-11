2c7bf35ebc0fe26f11babb9b25ed50f70cd95991
Joan Andrés <andres6936@live.com>
2020-08-10 19:23:46 -0500

    Added documentation

--- Include/Doryen/Graphics/Color/ColorTranslator.hpp
+++ Include/Doryen/Graphics/Color/ColorTranslator.hpp
@@ -16,8 +16,22 @@ namespace Doryen
 
 	private:
 
+		/**
+		 * To return value of a char. For example, 2 is  returned for '2'.
+		 * 10 is returned for 'A', 11 for 'B'.
+		 *
+		 * @param c Character to get the value in decimal.
+		 * @return The value of char in base 10 (decimal).
+		 */
 		static std::int32_t valueOfChar(char c);
 
+		/**
+		 * Function that convert a number in base (b) to base 10 (decimal).
+		 *
+		 * @param string Representation of number in another base (different of 10).
+		 * @param base Can be 2, 4, 8, 16
+		 * @return The number in base 10 (decimal).
+		 */
 		static std::int32_t toInt32(std::string_view string, std::uint8_t base);
 
 	public:
