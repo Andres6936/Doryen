f03fe0f070c79ebd14136ea2e338db51abf0f424
Joan Andrés <andres6936@live.com>
2020-08-10 19:10:32 -0500

    Modified test for allow null terminated and not null terminated

--- Test/Unit/TestColorParser.cpp
+++ Test/Unit/TestColorParser.cpp
@@ -7,9 +7,17 @@
 
 using namespace Doryen;
 
-TEST_CASE ("Parser color from hexadecimal")
+TEST_CASE ("Parser color from hexadecimal string literal (Null terminated)")
 {
 	Color c = Color::fromString("#192856");
+
+			CHECK(c.equals({ 25, 40, 86 }));
+}
+
+TEST_CASE ("Parser color from hexadecimal string variable (Not null terminated)")
+{
+	std::string hexadecimal = "#192856";
+	Color c = Color::fromString(hexadecimal);
 
 			CHECK(c.equals({ 25, 40, 86 }));
 }
