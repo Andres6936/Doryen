94fa7970749bd53dedaf198dc8ba08449f58d393
Joan Andrés <andres6936@live.com>
2020-08-15 22:46:23 -0500

    Added new file for work with the noise Fractional Brownian Motion

---
+++ Include/Doryen/Algorithms/Noise/FBM.hpp
@@ -0,0 +1,21 @@
+// Joan Andrés (@Andres6936) Github.
+
+#ifndef DORYEN_FRACTIONALBROWNIANMOTION_HPP
+#define DORYEN_FRACTIONALBROWNIANMOTION_HPP
+
+namespace Doryen
+{
+
+	/**
+	 * Fractional Brownian Motion
+	 */
+	class FBM
+	{
+
+	public:
+
+	};
+
+}
+
+#endif //DORYEN_FRACTIONALBROWNIANMOTION_HPP
---
+++ Source/Algorithms/Noise/FBM.cpp
@@ -0,0 +1,6 @@
+// Joan Andrés (@Andres6936) Github.
+
+#include "Doryen/FBM.hpp"
+
+using namespace Doryen;
+
