b95f4887087e70d3564c47523e2d5ad5958db2ca
Joan Andrés <andres6936@live.com>
2020-08-17 09:22:56 -0500

    Fixed problem with the function perlin Noise

--- Source/Algorithms/Noise/Perlin.cpp
+++ Source/Algorithms/Noise/Perlin.cpp
@@ -37,18 +37,26 @@
 using namespace Doryen;
 using namespace Doryen::Algorithms;
 
+const std::array permutation = {
+		151, 160, 137, 91, 90, 15,
+		131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23,
+		190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33,
+		88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166,
+		77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244,
+		102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196,
+		135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123,
+		5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42,
+		223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9,
+		129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228,
+		251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107,
+		49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
+		138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180
+};
+
 template<int Dimension>
 Perlin<Dimension>::Perlin()
 {
-	// Populate the vector in the range of [0, 256)
-	std::iota(p.begin(), p.begin() + 256, 0);
-
-	std::shuffle(std::begin(p), std::begin(p) + 256, std::default_random_engine{});
-
-	for (size_t i = 0; i < 256; ++i)
-	{
-		p[256 + i] = p[i];
-	}
+	for (int i = 0; i < 256; i++) p[256 + i] = p[i] = permutation[i];
 }
 
 template<int Dimension>
@@ -61,8 +69,8 @@ template<int Dimension>
 float Perlin<Dimension>::noise(const std::array<float, Dimension>& input)
 {
 	// Extract the values for x, y and z depended of dimension of Perlin
-	float x = (Dimension == 1) ? input[0] : 0.0f;
-	float y = (Dimension == 2) ? input[1] : 0.0f;
+	float x = (Dimension == 1 or Dimension == 2 or Dimension == 3) ? input[0] : 0.0f;
+	float y = (Dimension == 2 or Dimension == 3) ? input[1] : 0.0f;
 	float z = (Dimension == 3) ? input[2] : 0.0f;
 
 	const std::int32_t X = static_cast<std::int32_t>(std::floor(x)) & 255;
