0782834d2f7e017549ad2ffb79afa047c379fca7
Joan Andrés <andres6936@live.com>
2020-09-01 09:55:28 -0500

    Fixed indentation

--- Include/Doryen/Geometry/Point2D.hpp
+++ Include/Doryen/Geometry/Point2D.hpp
@@ -4,106 +4,103 @@
 #include <string>
 #include <cstdint>
 
-namespace Doryen
+namespace Doryen::Geometry
 {
-	namespace Geometry
+
+	/**
+	 * The Point2D class defines a point representing a location in (x,y)
+	 * coordinate space.
+	 *
+	 * @tparam T Arithmetic type: Integer, Real and Boolean
+	 */
+	template<typename T = std::int32_t>
+	class Point2D
 	{
 
-		/**
-		 * The Point2D class defines a point representing a location in (x,y)
-		 * coordinate space.
-		 *
-		 * @tparam T Arithmetic type: Integer, Real and Boolean
-		 */
-		template<typename T = std::int32_t>
-		class Point2D
+		static_assert(std::is_arithmetic_v<T>, "Only arithmetic types are allowed (Operations: +, -, /, *)");
+
+	public:
+
+		// Fields Public
+
+		T x;
+		T y;
+
+		// Constructs
+
+		Point2D() noexcept
 		{
+			x = T{};
+			y = T{};
+		}
 
-			static_assert(std::is_arithmetic_v<T>, "Only arithmetic types are allowed (Operations: +, -, /, *)");
+		Point2D(T _x, T _y) noexcept
+		{
+			x = _x;
+			y = _y;
+		}
 
-		public:
+		// Methods
 
-			// Fields Public
+		/**
+		 * Determines whether or not two points are equal. Two instances of
+		 * Point2D are equal if the values of their x and y member fields,
+		 * representing their position in the coordinate space, are the same.
+		 *
+		 * @param _rhs an object to be compared with this Point2D
+		 *
+		 * @return true if the object to be compared is an instance of
+		 *  Point2D and has the same values; false otherwise.
+		 */
+		bool equals(const Point2D<T>& _rhs) const noexcept
+		{
+			return this->x == _rhs.x and this->y == _rhs.y;
+		}
 
-			T x;
-			T y;
+		// Debug
 
-			// Constructs
+		[[maybe_unused]] std::string toString() const
+		{
+			return { '{' + std::to_string(x) + ", " + std::to_string(y) + '}' };
+		}
 
-			Point2D() noexcept
-			{
-				x = T{};
-				y = T{};
-			}
+		// Getters
 
-			Point2D(T _x, T _y) noexcept
-			{
-				x = _x;
-				y = _y;
-			}
+		/**
+		 * @return the X coordinate of this Point2D.
+		 */
+		T getX() const
+		{
+			return x;
+		}
 
-			// Methods
+		/**
+		 * @return the Y coordinate of this Point2D.
+		 */
+		T getY() const
+		{
+			return y;
+		}
 
-			/**
-			 * Determines whether or not two points are equal. Two instances of
-			 * Point2D are equal if the values of their x and y member fields,
-			 * representing their position in the coordinate space, are the same.
-			 *
-			 * @param _rhs an object to be compared with this Point2D
-			 *
-			 * @return true if the object to be compared is an instance of
-			 *  Point2D and has the same values; false otherwise.
-			 */
-			bool equals(const Point2D<T>& _rhs) const noexcept
-			{
-				return this->x == _rhs.x and this->y == _rhs.y;
-			}
+		// Setters
 
-			// Debug
+		/**
+		 * @param _x the new X coordinate of this Point2D
+		 */
+		void setX(T _x)
+		{
+			x = _x;
+		}
 
-			[[maybe_unused]] std::string toString() const
-			{
-				return { '{' + std::to_string(x) + ", " + std::to_string(y) + '}' };
-			}
+		/**
+		 * @param _y the new Y coordinate of this Point2D
+		 */
+		void setY(T _y)
+		{
+			y = _y;
+		}
 
-			// Getters
-
-			/**
-			 * @return the X coordinate of this Point2D.
-			 */
-			T getX() const
-			{
-				return x;
-			}
-
-			/**
-			 * @return the Y coordinate of this Point2D.
-			 */
-			T getY() const
-			{
-				return y;
-			}
-
-			// Setters
-
-			/**
-			 * @param _x the new X coordinate of this Point2D
-			 */
-			void setX(T _x)
-			{
-				x = _x;
-			}
-
-			/**
-			 * @param _y the new Y coordinate of this Point2D
-			 */
-			void setY(T _y)
-			{
-				y = _y;
-			}
-
-		};
-    }
+	};
 }
 
 #endif //LIBTCOD_POINT2D_HPP
