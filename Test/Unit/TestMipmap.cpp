7330860a321f8f57f39d1f713312b1d2c9f9a130
Joan Andrés <andres6936@live.com>
2021-01-05 15:15:07 -0500

    Added test for Mipmaps

---
+++ Test/Unit/TestMipmap.cpp
@@ -0,0 +1,14 @@
+// Joan Andrés (@Andres6936) Github.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+#include "Doryen/Image/Mipmap.hpp"
+
+using namespace Doryen;
+
+TEST_CASE("Size for default is {Width: 0, Height: 0}")
+{
+	Mipmap mipmap {};
+	CHECK(mipmap.getSize().equals({0, 0}));
+}
