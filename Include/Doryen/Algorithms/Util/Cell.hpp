f19722ea52f31491506d0beb752fe643956064ed
Adan <adan@maildrop.cc>
2021-01-29 22:28:02 -0500

    Added documentation

--- Include/Doryen/Algorithms/Util/Cell.hpp
+++ Include/Doryen/Algorithms/Util/Cell.hpp
@@ -3,14 +3,35 @@
 
 namespace Doryen
 {
-    class Cell
+
+	/**
+	 * The cell define a point a two-dimensional space, represent the current
+	 * state of that point, indicate whether it is traversable (walkable),
+	 * transparent or may be in the field of view of some entity.
+	 */
+	class Cell
 	{
 
 	public:
 
+		// Properties
+
+		/**
+		 * Define the property to see other cells through this cell.
+		 */
 		bool transparent = false;
+
+		/**
+		 * Define the possibility of traversing this cell.
+		 */
 		bool walkable = false;
+
+		/**
+		 * Define if a entity (usually the player) has in its field of view this cell.
+		 */
 		bool fov = false;
+
+		// Construct
 
 		Cell() noexcept = default;
 
