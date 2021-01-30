467e9c8b2122022d16497779109b70bf09f4eb67
Adan <adan@maildrop.cc>
2021-01-29 22:54:45 -0500

    Prepared test case for the class cell

---
+++ Test/Unit/TestCell.cpp
@@ -0,0 +1,13 @@
+// Joan Andrés (@Andres6936) Github.
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+#include "Doryen/Doryen.hpp"
+
+using namespace Doryen;
+
+TEST_CASE("")
+{
+
+}
