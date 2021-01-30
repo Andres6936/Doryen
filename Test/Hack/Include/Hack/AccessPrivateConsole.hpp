584b45049982fc7f6a32d8adc7cfba6ce8ce64b1
Joan Andrés <andres6936@live.com>
2021-01-30 09:42:47 -0500

    Added documentation

--- Test/Hack/Include/Hack/AccessPrivateConsole.hpp
+++ Test/Hack/Include/Hack/AccessPrivateConsole.hpp
@@ -5,19 +5,26 @@
 
 #include <Doryen/Doryen.hpp>
 
+// The follow code is based in: https://wandbox.org/permlink/ttKuVlrsMe2AycJo
+
+
 namespace Doryen::Test
 {
+	// Unique namespace in each TU.
 	namespace
 	{
+		// Unique type in each TU (internal linkage).
 		struct TranslationUnitTag {};
 	}
 
+	// 'Console::<any private member>' accessor.
 	template<typename UniqueTag, auto MemberPointer>
 	class AccessPrivateMemberConsole
 	{
 
 	public:
 
+		// (Injected) friend definition.
 		friend std::vector<Char>& accessPrivateMainBuffer(Console& console)
 		{
 			return console.*MemberPointer;
@@ -25,8 +32,10 @@ namespace Doryen::Test
 
 	};
 
+	// Friend (re-)declaration.
 	std::vector<Char>& accessPrivateMainBuffer(Console& console);
 
+	// Single explicit instantiation definition.
 	template struct AccessPrivateMemberConsole<TranslationUnitTag, &Console::buffer>;
 
 }
