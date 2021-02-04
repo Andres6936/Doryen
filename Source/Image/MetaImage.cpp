16f3d7060fe273d6824fcf222bf39a7d7d201204
Joan Andrés <andres6936@live.com>
2021-02-04 09:15:29 -0500

    Added documentation

--- Source/Image/MetaImage.cpp
+++ Source/Image/MetaImage.cpp
@@ -40,5 +40,8 @@ std::optional<Color> MetaImage::getKeyColor() const noexcept
 
 void MetaImage::setKeyColor(const Color& _keyColor)
 {
+	// It is recommended to use optional<T> in situations where there is exactly
+	// one, clear (to all parties) reason for having no value of type T, and
+	// where the lack of value is as natural as having any regular value of T
 	keyColor = std::make_optional(_keyColor);
 }
