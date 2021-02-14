8051ea2775e2ea23d5aeefebf2ecbeb82f3f70a0
Joan Andrés <andres6936@live.com>
2021-02-14 14:59:20 -0500

    Replaced old inclusion of headers

--- Samples/Ripples/UtilRipples.cpp
+++ Samples/Ripples/UtilRipples.cpp
@@ -24,7 +24,7 @@
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <string.h>
+#include <cstring>
 #include "Main.hpp"
 
 // dummy height indicating that a cell is not water
