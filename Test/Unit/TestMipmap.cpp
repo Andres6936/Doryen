e904ce6f979b2f298290f67ad52103b10d517148
Joan Andrés <andres6936@live.com>
2021-02-14 09:12:16 -0500

    Deleted workaround for avoid the fail build of workflow Github

--- Test/Unit/TestMipmap.cpp
+++ Test/Unit/TestMipmap.cpp
@@ -1,11 +1,7 @@
 // Joan Andrés (@Andres6936) Github.
 
-#define DOCTEST_CONFIG_USE_STD_HEADERS
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <string>
-#include <ostream>
-#include <iostream>
 #include "doctest/doctest.h"
 #include "Doryen/Image/Mipmap.hpp"
 
