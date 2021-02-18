df5d281892cafcbf02d00efe42cb9fcd1d8606a6
Joan Andrés <andres6936@live.com>
2021-02-17 22:12:22 -0500

    Added documentation

--- Source/Random/Number.cpp
+++ Source/Random/Number.cpp
@@ -18,6 +18,17 @@ float Random::Number::nextFloat(float from, float to)
 
 void Random::Number::setRandomSeed()
 {
+	// The class std::random_device this is a special class which produces
+	// uniformly-distributed unsigned integers with 32 bits of length. It can
+	// produce random numbers either by accessing the operational system's
+	// entropy pool via system calls or by using hardware random number
+	// generators such as Intel's RdRand when available (but not all
+	// implementations of std::random_device allow this). Developers must be
+	// warned, however, that even though 32-bit unsigned integers are generated,
+	// the entropy of this random number generator may be lower than 32 bits.
+
+	// Reference: https://diego.assencio.com/?index=6890b8c50169ef45b74db135063c227c
+
 	// Rewrite the random engine with a new, but a difference of
 	// original, this new random engine have a random device
 	// that generate pseudo-random numbers
