0000da4b4d3fc37ee6241e7a7b3293c4ae814a47
Joan Andrés <andres6936@live.com>
2021-04-17 16:37:25 -0500

    Inheritance MetaImage

--- Include/Doryen/Renderer/Dummy/ImageDummy.hpp
+++ Include/Doryen/Renderer/Dummy/ImageDummy.hpp
@@ -3,10 +3,17 @@
 #ifndef DORYEN_IMAGEDUMMY_HPP
 #define DORYEN_IMAGEDUMMY_HPP
 
+#include <Doryen/Image/MetaImage.hpp>
+
 namespace Doryen
 {
 
-	class ImageDummy
+	/**
+	 * Represent two things, a struct that content an representation
+	 * of an image or a vector that content a representation of pixels
+	 * that represent an image.
+	 */
+	class ImageDummy : public MetaImage
 	{
 
 	public:
